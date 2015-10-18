d97cf2f0ae2d32d5fb3a33da9c22592ce7a48b62
thedax <daxtsu@gmail.com>
2015-10-17 20:03:16 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+<b><center><h1>3DS Quick Shutdown</h></center></b>
+==========
+
+This app will shut down your 3DS upon running it. If you don't want to shutdown, just hold any button and it'll exit immediately.
+
+Requires [devkitARM](http://sourceforge.net/projects/devkitpro/files/devkitARM/) to build.
+
+Based on https://github.com/thedax/3DSHomebrewTemplate. Shutdown code adapted from https://github.com/Cpasjuste/CtrBootManager.
---
+++ source/main.cpp
@@ -0,0 +1,35 @@
+#include <3ds.h>
+#include <stdio.h>
+
+void shutdown3DS()
+{
+	Handle ptmSysmHandle = 0;
+	Result result = srvGetServiceHandle(&ptmSysmHandle, "ptm:sysm");
+	if (result != 0)
+		return;
+
+	u32 *commandBuffer = getThreadCommandBuffer();
+	commandBuffer[0] = 0x040700C0; //ptm:s::ShutdownAsync
+	commandBuffer[1] = 0;
+	commandBuffer[2] = 0;
+	commandBuffer[3] = 0;
+
+	svcSendSyncRequest(ptmSysmHandle);
+	svcCloseHandle(ptmSysmHandle);
+}
+
+int main(int argc, char **argv) {
+    hidScanInput();
+        
+	// If any key is pressed, cancel the shutdown.
+	if (hidKeysDown() != 0)
+		goto done;
+
+	shutdown3DS();
+    
+done:
+	// Hack: the 3ds crashes ("An error has occcurred.") for some reason
+	// without one iteration of the main loop.
+	aptMainLoop();
+    return 0;
+}
